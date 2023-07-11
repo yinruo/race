b2fbaf3d6dfcca08392c7550b5f67744b2a90e50
yinruo <yinruoh@gmail.com>
2023-07-11 15:04:27 +0200

    add controller

---
+++ race/webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/comp/smartfilterbar/SmartFilterBar",
+    "sap/m/ComboBox"
+], function(MessageToast,Filter, SmartFilterBar, ComboBox) {
+    'use strict';
+
+    return {
+        onEmailPress: function (oEvent) {
+            console.log('email pressed')
+            var oButton = oEvent.getSource();
+            var oContext = oButton.getBindingContext();
+            var sEmail = oContext.getProperty("CEmail");
+            window.location.href = "mailto:" + sEmail;
+        }
+    };
+});
