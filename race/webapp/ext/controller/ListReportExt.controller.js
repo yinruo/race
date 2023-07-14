bc3ca5e27d25e84b136a8aaaa57a01cb4caae00c
Marina Sukhanova <m.sukhanova@tum.de>
2023-07-14 20:54:41 +0200

    .

--- race/webapp/ext/controller/ListReportExt.controller.js
+++ race/webapp/ext/controller/ListReportExt.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
     return {
         onEmailPress: function (oEvent) {
             var oButton = oEvent.getSource();
-            var oTable = oButton.getParent().getParent(); // Assuming the button is in the table header
+            var oTable = oButton.getParent().getParent(); 
             var aSelectedItems = oTable.getSelectedItems();
             
             if (aSelectedItems.length === 0) {
@@ -22,6 +22,21 @@ sap.ui.define([
             var sEmail = oContext.getProperty("CEmail");
             var sName = oContext.getProperty("CName");
             sap.m.URLHelper.triggerEmail(sEmail, "Request: Application and CV needed for potential job offer", "Dear " + sName + ",");
+        },
+        onCertificatePress: function(oEvent) {
+            var oButton = oEvent.getSource();
+            var oTable = oButton.getParent().getParent(); 
+            var aSelectedItems = oTable.getSelectedItems();
+            
+            if (aSelectedItems.length === 0) {
+                MessageToast.show("Please select an item from the table.");
+                return;
+            }
+
+            var oSelectedItem = aSelectedItems[0]; // Assuming only one item can be selected
+            var oContext = oSelectedItem.getBindingContext();
+            var certiLink = oContext.getProperty("CertiLink");
+            window.open(certiLink);
         }
     };
 });
