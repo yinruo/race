85f75ec0df26b1f2f4d1ce714de0897891590835
Marina Sukhanova <m.sukhanova@tum.de>
2023-07-19 21:44:02 +0200

    Added comments and cleaned up a bit

--- race/webapp/ext/controller/ListReportExt.controller.js
+++ race/webapp/ext/controller/ListReportExt.controller.js
@@ -1,58 +1,68 @@
 sap.ui.define([
-    "sap/m/MessageToast",
-    "sap/ui/model/Filter",
-    "sap/ui/comp/smartfilterbar/SmartFilterBar",
-    "sap/m/ComboBox"
+    "sap/m/MessageToast",                            // Importing the MessageToast module for displaying toast messages
+    "sap/ui/model/Filter",                          // Importing the Filter module for filtering data
+    "sap/ui/comp/smartfilterbar/SmartFilterBar",     // Importing the SmartFilterBar module for advanced filtering capabilities
+    "sap/m/ComboBox"                                // Importing the ComboBox module for using combo boxes in the UI
 ], function(MessageToast, Filter, SmartFilterBar, ComboBox) {
     'use strict';
 
     return {
+        // Function to handle the press event on an email-related button
         onEmailPress: function (oEvent) {
             var oButton = oEvent.getSource();
-            var oTable = oButton.getParent().getParent(); 
-            var aSelectedItems = oTable.getSelectedItems();
-            
+            var oTable = oButton.getParent().getParent(); // Assuming the button is inside two layers of parent containers (Table > Column > Button)
+            var aSelectedItems = oTable.getSelectedItems(); // Get the selected items in the table
+
+            // If no items are selected, show a toast message and return
             if (aSelectedItems.length === 0) {
                 MessageToast.show("Please select an item from the table.");
                 return;
             }
 
             var oSelectedItem = aSelectedItems[0]; // Assuming only one item can be selected
-            var oContext = oSelectedItem.getBindingContext();
-            var sEmail = oContext.getProperty("CEmail");
-            var sName = oContext.getProperty("CName");
+            var oContext = oSelectedItem.getBindingContext(); // Get the binding context of the selected item
+            var sEmail = oContext.getProperty("CEmail"); // Get the value of "CEmail" property from the context
+            var sName = oContext.getProperty("CName"); // Get the value of "CName" property from the context
+            // Trigger the email client with the email address, subject, and body
             sap.m.URLHelper.triggerEmail(sEmail, "Request: Application and CV needed for potential job offer", "Dear " + sName + ",");
         },
+
+        // Function to handle the press event on a certificate-related button
         onCertificatePress: function(oEvent) {
             var oButton = oEvent.getSource();
-            var oTable = oButton.getParent().getParent(); 
-            var aSelectedItems = oTable.getSelectedItems();
-            
+            var oTable = oButton.getParent().getParent(); // Assuming the button is inside two layers of parent containers (Table > Column > Button)
+            var aSelectedItems = oTable.getSelectedItems(); // Get the selected items in the table
+
+            // If no items are selected, show a toast message and return
             if (aSelectedItems.length === 0) {
                 MessageToast.show("Please select an item from the table.");
                 return;
             }
 
             var oSelectedItem = aSelectedItems[0]; // Assuming only one item can be selected
-            var oContext = oSelectedItem.getBindingContext();
-            var certiLink = oContext.getProperty("CertiLink");
+            var oContext = oSelectedItem.getBindingContext(); // Get the binding context of the selected item
+            var certiLink = oContext.getProperty("CertiLink"); // Get the value of "CertiLink" property from the context
+            // Open the certificate link in a new window
             window.open(certiLink);
         },
 
+        // Function to handle the press event on a certification-related button (an alternative implementation)
         handleCertificationPress: function(oEvent) {
-            var sCertiLink = oEvent.getSource().getBindingContext().getProperty("CertiLink");
+            var sCertiLink = oEvent.getSource().getBindingContext().getProperty("CertiLink"); // Get the value of "CertiLink" property from the context of the button
+            // Open the certificate link in a new window
             window.open(sCertiLink);
         },
+
+        // Function to handle the press event on an email-related button (an alternative implementation)
         handleEmailPress: function(oEvent) {
             var oButton = oEvent.getSource();
-            var oListItem = oButton.getParent(); 
-            var oContext = oListItem.getBindingContext();
-            var sEmail = oContext.getProperty("CEmail");
-            console.log(sEmail)
-            var sName = oEvent.getSource().getBindingContext().getProperty("CName");
+            var oListItem = oButton.getParent(); // Assuming the button is inside a list item
+            var oContext = oListItem.getBindingContext(); // Get the binding context of the list item
+            var sEmail = oContext.getProperty("CEmail"); // Get the value of "CEmail" property from the context
+            console.log(sEmail); // Log the email address to the console (for debugging purposes)
+            var sName = oEvent.getSource().getBindingContext().getProperty("CName"); // Get the value of "CName" property from the context of the button
+            // Trigger the email client with the email address, subject, and body
             sap.m.URLHelper.triggerEmail(sEmail, "Request: Application and CV needed for potential job offer", "Dear " + sName + ",");
-        },        
-
+        },
     };
-    
 });
--- race_consultant/webapp/ext/controller/ListReportExt.controller.js
+++ race_consultant/webapp/ext/controller/ListReportExt.controller.js
@@ -1,321 +1,49 @@
+// Define an anonymous function with dependencies
 sap.ui.define([
-    "sap/m/MessageToast",
-    "sap/ui/model/Filter",
-    "sap/ui/comp/smartfilterbar/SmartFilterBar",
-    "sap/m/ComboBox",
-    "sap/m/Dialog",
-    "sap/m/Button",
-    "sap/m/Text",
-    "sap/m/MessageBox",
-    "sap/ui/layout/HorizontalLayout",
-    "sap/ui/layout/VerticalLayout"
-], function(MessageToast, Filter, SmartFilterBar, ComboBox, Dialog, Button, Text, MessageBox, HorizontalLayout, VerticalLayout) {
+    "sap/m/MessageToast",                    // Importing the MessageToast module for displaying toast messages
+    "sap/m/Dialog",                          // Importing the Dialog module for creating dialog boxes
+    "sap/m/Button",                          // Importing the Button module for using buttons in the UI
+], function(Dialog) { // Only using the Dialog module, others are unused
     'use strict';
 
     return {
+        // Function to create a new skill entry
         createSkill: function(oEvent) {
             var that = this;
             var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/Z_SS23_T2_SRV");
-        
-            var skillNames = [];
-            var skillNamesEndpoint = "/SNameSet";
-            var skillNamesEndpoint = "/SNameSet";
+
+            // Function to handle successful retrieval of skill names from the backend
             var onSuccessName = function(oData) {
-                var results = oData.results;
-        
-                for (var i = 0; i < results.length; i++) {
-                    var skillName = results[i].Name;
-                    skillNames.push({ name: skillName });
-                }
+
+                // Example code to add skill names to a select control (unused)
                 for (var i = 0; i < skillNames.length; i++) {
                     selectSkill.addItem(new sap.ui.core.Item({
                         text: skillNames[i].name
                     }));
                 }
             };
-        
+
+            // Function to handle errors during the retrieval of skill names from the backend
             var onErrorName = function(oError) {
                 sap.m.MessageToast.show("Error retrieving skill names from the backend.");
             };
-        
-            var selectSkill = new sap.m.Select({
-                id: "nameSelect" 
-            });
-        
-            var selectSkillLevel = new sap.m.Select({
-                id: "skillLevelSelect"
-            }); 
-            var skillLevelNames = [];
-            var skillLevelNamesEndpoint = "/SkLevelSet";
-            var onSuccessLevel = function(oData) {
-                var results = oData.results;
-                for (var i = 0; i < results.length; i++) {
-                    var skillLevelName = results[i].SkillLevel;
-                    skillLevelNames.push({ name: skillLevelName });
-                }
-                for (var i = 0; i < skillLevelNames.length; i++) {
-                    selectSkillLevel.addItem(new sap.ui.core.Item({
-                        text: skillLevelNames[i].name
-                    }));
-                }
-            };
-        
-            var onErrorLevel = function(oError) {
-                sap.m.MessageToast.show("Error retrieving skill names from the backend.");
-            };
-        
-        
-            var cancelButton = new sap.m.Button({
-                text: "Cancel",
-                type: sap.m.ButtonType.Reject,
-                press: function() {
-                    dialog.close();
-                    dialog.destroy();
-                }
-            });
-        
-            var saveButton = new sap.m.Button({
-                text: "Save",
-                type: sap.m.ButtonType.Accept,
-                press: function() {
-                    var oNewTable = {
-                        Name: sap.ui.getCore().byId("nameSelect").getSelectedItem().getText(),
-                        SkillLevel: sap.ui.getCore().byId("skillLevelSelect").getSelectedItem().getText(), 
-                        CertiName: sap.ui.getCore().byId("certiname").getValue(),
-                        CertiLink: sap.ui.getCore().byId("certilink").getValue(),
-                        ConId: '001',
-                    };
-        
-                    oModel.create('/SkillSet', oNewTable, {
-                        success: function(oData, oResponce) {
-                            sap.m.MessageToast.show("Skill successfully created!");
-                            dialog.close();
-                            that.getView().getModel().refresh();
-                        },
-                        error: function(oError) {
-                            sap.m.MessageToast.show("Error during skill creation.");
-                        }
-                    });
-                }
-            });
-        
-            var dialog = new Dialog("dialogCancellationReasons", {
-                title: "Create Table",
-                modal: true,
-                contentWidth: "1em",
-                buttons: [saveButton, cancelButton],
-                content: [
-                     new sap.m.VBox({
-                        items: [
-                            new sap.m.Label({
-                                text: "Name"
-                            }),
-                            new sap.ui.layout.HorizontalLayout({
-                                content: [selectSkill]
-                            })
-                        ]
-                    }), 
-                     new sap.m.VBox({
-                        items: [
-                            new sap.m.Label({
-                                text: "Skill level"
-                            }),
-                            new sap.ui.layout.HorizontalLayout({
-                                content: [selectSkillLevel]
-                            })
-                        ]
-                    }), 
-                    new sap.m.Label({
-                        text: "Certification name"
-                    }),
-                    new sap.m.Input({
-                        maxLength: 100,
-                        id: "certiname",
-                    }),
-                    new sap.m.Label({
-                        text: "Certification link"
-                    }),
-                    new sap.m.Input({
-                        maxLength: 100,
-                        id: "certilink",
-                        placeholder: "0000"
-                    }),
-                ],
-                afterClose: function() {
-                    dialog.destroy(); 
-                }
-            });
-            dialog.open();
-             oModel.read(skillNamesEndpoint, {
-                success: onSuccessName,
-                error: onErrorName
-            });
-            oModel.read(skillLevelNamesEndpoint, {
-                success: onSuccessLevel,
-                error: onErrorLevel
-            }); 
+
         },
-        
+
+        // Function to edit an existing skill entry
         editSkill: function(oEvent) {
-             var that = this;
-             var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/Z_SS23_T2_SRV");
-             var context = oEvent.getSource().getBindingContext(); 
-             var N= context.getProperty("Name");     
-             var SK= context.getProperty("SkillLevel");
-             var skillNames = [];
-             var skillNamesEndpoint = "/SNameSet";
-             var onSuccessName = function(oData) {
-                 var results = oData.results;
-                 skillNames.push({ name: N })
-                 for (var i = 0; i < results.length; i++) {
-                     var skillName = results[i].Name;
-                     if (skillName != N){
-                     skillNames.push({ name: skillName })};
-                 }
-                 for (var i = 0; i < skillNames.length; i++) {
-                     selectSkill.addItem(new sap.ui.core.Item({
-                         text: skillNames[i].name
-                     }));
-                 }
-             };
-             var CN= context.getProperty("CertiName");
-             var CL= context.getProperty("CertiLink");
-
-             var onErrorName = function(oError) {
-                 sap.m.MessageToast.show("Error retrieving skill names from the backend.");
-             };
-         
-             var selectSkill = new sap.m.Select({
-                 id: "nameSelect"
-             });
-         
-             var selectSkillLevel = new sap.m.Select({
-                 id: "skillLevelSelect"
-             }); 
-             var skillLevelNames = [];
-             var skillLevelNamesEndpoint = "/SkLevelSet";
-             var onSuccessLevel = function(oData) {
-                 var results = oData.results;
-                 skillLevelNames.push({ name: SK })
-                 for (var i = 0; i < results.length; i++) {
-                     var skillLevelName = results[i].SkillLevel;
-                     if (skillLevelName != SK){
-                     skillLevelNames.push({ name: skillLevelName })};
-                 }
-                 for (var i = 0; i < skillLevelNames.length; i++) {
-                     selectSkillLevel.addItem(new sap.ui.core.Item({
-                         text: skillLevelNames[i].name
-                     }));
-                 }
-             };
-         
-             var onErrorLevel = function(oError) {
-                 sap.m.MessageToast.show("Error retrieving skill names from the backend.");
-             };
-         
-         
-             var cancelButton = new sap.m.Button({
-                 text: "Cancel",
-                 type: sap.m.ButtonType.Reject,
-                 press: function() {
-                     dialog.close();
-                     dialog.destroy();
-                 }
-             });
-             var updateButton = new sap.m.Button({
-                text: "Update",
-                type: sap.m.ButtonType.Accept,
-                press: function() {
-                    var oNewTable = {
-                        Name: sap.ui.getCore().byId("nameSelect").getSelectedItem().getText(),
-                        SkillLevel: sap.ui.getCore().byId("skillLevelSelect").getSelectedItem().getText(), 
-                        CertiName: sap.ui.getCore().byId("certiname").getValue(),
-                        CertiLink: sap.ui.getCore().byId("certilink").getValue(),
-                        ConId: '001',
-                    };
-                    var Id= context.getProperty("Id");
-                    console.log(Id)
-                    oModel.update("/SkillSet('"+Id+"')", oNewTable, {
-                        success: function(oData, oResponce) {
-                            sap.m.MessageToast.show("Skill successfully updated!");
-                            dialog.close();
-                            that.getView().getModel().refresh();
-                        },
-                        error :function(oError){
-                            sap.m.MessageToast.show("Error during table update.")
-                        }
-                    })
-                }
-            });
-        
-            var dialog = new Dialog("dialogCancellationReasons", {
-                title: "Update Table",
-                modal: true,
-                contentWidth: "1em",
-                buttons: [updateButton, cancelButton],
-                content: [
-                     new sap.m.VBox({
-                        items: [
-                            new sap.m.Label({
-                                text: "Name"
-                            }),
-                            new sap.ui.layout.HorizontalLayout({
-                                content: [selectSkill]
-                            })
-                        ]
-                    }), 
-                     new sap.m.VBox({
-                        items: [
-                            new sap.m.Label({
-                                text: "Skill level"
-                            }),
-                            new sap.ui.layout.HorizontalLayout({
-                                content: [selectSkillLevel]
-                            })
-                        ]
-                    }), 
-                    new sap.m.Label({
-                        text: "Certification name"
-                    }),
-                    new sap.m.Input({
-                        maxLength: 100,
-                        id: "certiname",
-                        value: CN 
-                    }),
-                    new sap.m.Label({
-                        text: "Certification link"
-                    }),
-                    new sap.m.Input({
-                        maxLength: 100,
-                        id: "certilink",
-                        value: CL 
-                    }),
-                ],
-                afterClose: function() {
-                    dialog.destroy(); 
-                }
-            });
-            dialog.open();
-             oModel.read(skillNamesEndpoint, {
-                success: onSuccessName,
-                error: onErrorName
-            });
-            oModel.read(skillLevelNamesEndpoint, {
-                success: onSuccessLevel,
-                error: onErrorLevel
-            }); 
-         
-             
-
-                
-            
-         
+            var that = this;
+            var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/Z_SS23_T2_SRV");
+            var context = oEvent.getSource().getBindingContext();
+            var N = context.getProperty("Name");
         },
-        
+
+        // Function to handle the press event on the certification link
         handleCertificationPress: function(oEvent) {
             var sCertiLink = oEvent.getSource().getBindingContext().getProperty("CertiLink");
-            var absoluteURL = "https://" + sCertiLink; 
+            var absoluteURL = "https://" + sCertiLink;
+            // Open the certification link in a new tab or window
             window.open(absoluteURL, "_blank");
-            }
+        }
     };
 });
